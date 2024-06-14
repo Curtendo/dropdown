f9615a0614c2a903225c9e114885596439e9528e
Curtis CHROME <mrmr450@gmail.com>
2024-06-14 11:41:41 +0900

    Allow dropdown logic on multiple dropdowns

--- src/index.js
+++ src/index.js
@@ -3,18 +3,27 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 class Dropdown {
   constructor() {
-    this.hamburgerMenu = document.querySelector('.hamburger-menu');
+    this.hamburgerMenus = document.querySelectorAll('.hamburger-menu');
+    this.dropdowns = document.querySelectorAll('.dropdown');
 
     this.initEventListeners();
   }
 
   initEventListeners() {
-    this.hamburgerMenu.addEventListener('click', (e) => {
-      this.handleDropdownDisplay(e);
+    this.hamburgerMenus.forEach((menu) => {
+      menu.addEventListener('click', (e) => {
+        this.handleDropdownDisplay(e);
+      });
     });
   }
 
   handleDropdownDisplay(e) {
+    // Hide all dropdowns
+    this.dropdowns.forEach((dropdown) => {
+      dropdown.hidden = true;
+    });
+
+    // Show only the clicked dropdown
     const dropdownMenu = e.target.nextSibling.nextSibling;
     dropdownMenu.hidden = !dropdownMenu.hidden;
   }
