65f000d6b687ff1dd36e1de1353cb4dd7ac9e50a
Curtis CHROME <mrmr450@gmail.com>
2024-06-14 11:26:59 +0900

    Add hamburger menu logic for one menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+class Dropdown {
+  constructor() {
+    this.hamburgerMenu = document.querySelector('.hamburger-menu');
+
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    this.hamburgerMenu.addEventListener('click', (e) => {
+      this.handleDropdownDisplay(e);
+    });
+  }
+
+  handleDropdownDisplay(e) {
+    const dropdownMenu = e.target.nextSibling.nextSibling;
+    dropdownMenu.hidden = !dropdownMenu.hidden;
+  }
+}
+
+const dropdown = new Dropdown();
